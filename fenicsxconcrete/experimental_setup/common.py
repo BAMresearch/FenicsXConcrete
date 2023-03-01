7b94bc3e200de01f55d619168b2ff5af7660f667
darcones <daniel.andres-arcones@bam.de>
2023-03-01 14:15:52 +0100

    Added Philipp's implementation of BCs

---
+++ fenicsxconcrete/experimental_setup/common.py
@@ -0,0 +1,13 @@
+"""Based on Philipp Diercks implementation for multi"""
+"""common helpers"""
+
+
+def to_floats(values):
+    floats = []
+    try:
+        for v in values:
+            floats.append(float(v))
+    except TypeError as e:
+        floats = [float(values)]
+
+    return floats
