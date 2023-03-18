61d57feb9d3482574fe21bb93f188d272b604723
Philipp Diercks <philipp.diercks@bam.de>
2023-03-18 19:28:30 +0100

    [__init__.py] set default log level

--- src/fenicsxconcrete/__init__.py
+++ src/fenicsxconcrete/__init__.py
@@ -1,3 +1,17 @@
 import logging
 
 logging.getLogger("fenicsxconcrete")
+
+
+def set_log_levels(levels: dict = None) -> None:
+    """Sets log levels for loggers.
+
+    Args:
+      levels: The names of the loggers and log level to be set.
+    """
+    levels = levels or {"fenicsxconcrete": logging.INFO}
+    for k, v in levels.items():
+        logging.getLogger(k).setLevel(v)
+
+
+set_log_levels()
