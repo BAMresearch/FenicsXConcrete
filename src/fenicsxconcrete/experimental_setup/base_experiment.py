7dbaed4f20b096d69f05c4eb4d6c1c0de9328f35
aradermacher <annika.robens-radermacher@bam.de>
2023-03-20 16:15:48 +0100

    removed unnecessary future imports

--- src/fenicsxconcrete/experimental_setup/base_experiment.py
+++ src/fenicsxconcrete/experimental_setup/base_experiment.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 
 import dolfinx as df
 import numpy as np
--- src/fenicsxconcrete/finite_element_problem/base_material.py
+++ src/fenicsxconcrete/finite_element_problem/base_material.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+
 import dolfinx as df
 from pathlib import PosixPath, Path
 import sys
