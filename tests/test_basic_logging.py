ee78a659380d5af8e8df102f37638f73eebba46e
Philipp Diercks <philipp.diercks@bam.de>
2023-03-20 21:16:03 +0100

    [test_basic_logging] turn print statements into assertions

--- tests/test_basic_logging.py
+++ tests/test_basic_logging.py
@@ -30,10 +30,10 @@ def test_set_log_levels():
 
 
 def test_fenicsx_loggers():
+    """application specific settings for FEniCSx"""
 
-    # application specific settings for FEniCSx
+    # ### ufl and ffcx
     ufl_logger = ufl.log.get_logger()
-
     # it seems per default the levels are
     # ufl: DEBUG (10)
     # ffcx: WARNIG (30)
@@ -47,18 +47,7 @@ def test_fenicsx_loggers():
     assert ffcx.logger.getEffectiveLevel() == logging.ERROR
     assert ufl_logger.getEffectiveLevel() == logging.CRITICAL
 
-    # note that dolfinx.log.LogLevel has levels ERROR, OFF, WARNING and INFO
-    # and that the integer values do not follow the convention of the stdlib
-    # logging
-    dfx_levels = [
-        dolfinx.log.LogLevel.INFO,
-        dolfinx.log.LogLevel.WARNING,
-        dolfinx.log.LogLevel.OFF,
-        dolfinx.log.LogLevel.ERROR,
-    ]
-    for lvl in dfx_levels:
-        print(f"Log level {lvl} has value: {lvl.value}.")
-
+    # ### dolfinx
     initial_level = dolfinx.log.get_log_level()
     assert initial_level.value == -1  # WARNING
 
@@ -73,6 +62,19 @@ def test_fenicsx_loggers():
     dolfinx.log.set_log_level(dolfinx.log.LogLevel.ERROR)
     current_level = dolfinx.log.get_log_level()
     assert current_level.value == -2
+
+    # note that dolfinx.log.LogLevel has levels INFO, WARNING, ERROR and OFF
+    # and that the integer values do not follow the convention of the stdlib
+    # logging
+    dfx_levels = [
+        (dolfinx.log.LogLevel.INFO, 0),
+        (dolfinx.log.LogLevel.WARNING, -1),
+        (dolfinx.log.LogLevel.ERROR, -2),
+        (dolfinx.log.LogLevel.OFF, -9),
+    ]
+    for lvl, value in dfx_levels:
+        dolfinx.log.set_log_level(lvl)
+        assert dolfinx.log.get_log_level().value == value
 
 
 if __name__ == "__main__":
