9e0ab33c936e4da5dbfcfd536ae089106ea199c6
srosenbu <sjard-mathis.rosenbusch@bam.de>
2024-09-20 13:34:46 +0200

    Remove old code

--- src/fenicsxconcrete/finite_element_problem/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_material import MaterialProblem, QuadratureFields, SolutionFields
-from .concrete_am import ConcreteAM, ConcreteThixElasticModel
-from .concrete_thermo_mechanical import ConcreteThermoMechanical
-from .linear_elasticity import LinearElasticity
--- src/fenicsxconcrete/util/__init__.py
+++
@@ -1,6 +0,0 @@
-from .logger import LogMixin
-from .mori_tanaka_homogenization import ConcreteHomogenization
-from .parameters import Parameters
-from .projection import project
-from .quadrature import QuadratureEvaluator, QuadratureRule
-from .unit_registry import ureg
