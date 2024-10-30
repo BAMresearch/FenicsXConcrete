802cd02afacf5b0996778e58e6191271279c4189
srosenbu <sjard-mathis.rosenbusch@bam.de>
2024-10-30 14:49:51 +0100

    WIP

--- src/fenicsxconcrete/bcs.py
+++ src/fenicsxconcrete/bcs.py
@@ -1,11 +1,26 @@
 
-from pydantic import ConfigDict, RootModel
+from pydantic import ConfigDict, RootModel, field_serializer
 from pydantic.dataclasses import dataclass
 from pydantic.types import conlist
-from typing import Callable, NewType
+from typing import Annotated, Callable, NewType
 import numpy as np
+from pint import Unit
+from names import SolutionField
 
-Marker = NewType('Marker', int)
+import inspect
+
+from pydantic.functional_serializers import PlainSerializer
+
+def marker_serializer(marker: int | np.ndarray | Callable[[np.ndarray], np.ndarray], _info):
+    if isinstance(marker, np.ndarray):
+        return str(list(marker))
+    elif callable(marker):
+        return inspect.getsource(marker)
+    elif isinstance(marker, int):
+        return str(marker)
+
+Marker = Annotated[int | Callable[[np.ndarray], np.ndarray], PlainSerializer(marker_serializer)]
+
 
 @dataclass(config=dict(arbitrary_types_allowed=True))
 class DirichletBCDefinition:
@@ -13,24 +28,32 @@ class DirichletBCDefinition:
     Definition of a time- and position-dependent Dirichlet boundary condition.
     Note that functions cannot be serialized to JSON!
     """
-    marker: Marker | list[float] | Callable[[np.ndarray], np.ndarray]
-    value: conlist(float, min_length=1, max_length=3) | Callable[[np.ndarray, float], np.ndarray]
+    marker: Marker
+    value: np.ndarray | Callable[[np.ndarray, float], np.ndarray]
+    unit: Unit
     subspace: int | None
-    variable: str
+    variable: SolutionField
+
 
 @dataclass(config=dict(arbitrary_types_allowed=True))
 class NeumannBCDefinition:
     """
     Definition of a time- and position-dependent Neumann boundary condition.
     """
-    marker: Marker
-    value: conlist(float, min_length=1, max_length=3) | Callable[[np.ndarray, float], np.ndarray] 
+    marker: Marker | Callable[[np.ndarray], np.ndarray]
+    value: list[float] | Callable[[np.ndarray, float], np.ndarray] 
 
 @dataclass(config=dict(arbitrary_types_allowed=True))
-class BodyForceDefinition:
-    value: conlist(float, min_length=1, max_length=3)
+class BoundaryNormalTermDefinition:
+    value: list[float] | Callable[[np.ndarray, float], np.ndarray]
+    variable: str
+
+@dataclass(config=dict(arbitrary_types_allowed=True))
+class SourceTermDefinition:
+    value: list[float] | Callable[[np.ndarray, float], np.ndarray]
+    variable: str
 
 @dataclass(config=dict(arbitrary_types_allowed=True))
 class InitialConditionDefinition:
-    value: list[float]
+    value: list[float] | Callable[[np.ndarray], np.ndarray]
     variable: str
--- src/fenicsxconcrete/experiment.py
+++ src/fenicsxconcrete/experiment.py
@@ -1,10 +1,12 @@
-from pydantic import RootModel
+from typing import Literal
+import numpy as np
+from pydantic import Field, RootModel
 from bcs import DirichletBCDefinition, InitialConditionDefinition, NeumannBCDefinition, BodyForceDefinition
 from mesh import MeshGenerator
 from material import MaterialDefinition, LinearElasticMaterial
-from pydantic.dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
-@dataclass(config=dict(arbitrary_types_allowed=True))
+@dataclass#(config=dict(arbitrary_types_allowed=True))
 class Experiment:
     dirichlet_bcs: list[DirichletBCDefinition] | None
     neumann_bcs: list[NeumannBCDefinition] | None
@@ -14,13 +16,15 @@ class Experiment:
     solution_fields: list[str]
     time: tuple[float, float]
     material: MaterialDefinition
-    name: str = "experiment"
+    name: str ='experiment'
 
 
 
 
 if __name__ == "__main__":
-    bc = DirichletBCDefinition(marker=1, value=[0.,0.], subspace=0, variable='displacement')
+    def marker_function(x):
+        return np.isclose(x[1], 0)
+    bc = DirichletBCDefinition(marker=marker_function, value=np.array([0.,0.]), subspace=0, variable='displacement')
     neumann = NeumannBCDefinition(marker=2, value=[-42.0], variable='displacement')
     initial = InitialConditionDefinition(value=[42.24], variable='density')
     body_force = BodyForceDefinition(value=[0.,0., 9.81], variable='displacement')
@@ -30,6 +34,6 @@ if __name__ == "__main__":
     time = (0., 1.)
     exp = Experiment(dirichlet_bcs=[bc], neumann_bcs=[neumann], initial_conditions=[initial], body_forces=[body_force], geometry=geo, solution_fields=solution_fields, time=time, material=mat)
 
-
-    print( RootModel[Experiment](exp).model_dump_json(indent=4))
+    print(asdict(exp))
+    print(RootModel[Experiment](exp).model_dump_json(indent=4))
     
